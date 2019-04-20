75e1c47faac951f1402bc1a1ff9c76ccb28fa67a
martincz <martincz@mokeedev.com>
2019-04-20 15:32:17 +0800

    MKCenter2: Fix require permissions
    Change-Id: Icb1c3e64f2964b9619f5b4230039fe2476d8d6f8
    (cherry picked from commit 6a244903c5c25b3da0d39a31e04727c3c586ac26)

--- app/src/main/java/com/mokee/center/activity/MainActivity.java
+++ app/src/main/java/com/mokee/center/activity/MainActivity.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 2018 The MoKee Open Source Project
+ * Copyright (C) 2018-2019 The MoKee Open Source Project
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -17,9 +17,13 @@
 
 package com.mokee.center.activity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.annotation.StringRes;
 
@@ -37,6 +41,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 import com.mokee.center.MKCenterApplication;
 import com.mokee.center.R;
@@ -53,7 +58,131 @@ import static com.mokee.center.misc.Constants.DONATION_RESULT_SUCCESS;
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
 
+    private final int PERMISSION_REQUEST_CODE = 1;
+
+    private int mIndexPermissionRequestStorageWrite = 0;
+    private int mIndexPermissionRequestStorageRead = 0;
+    private int mIndexPermissionRequestPhone = 0;
+
+    private boolean mShouldRequestStoragePermission = false;
+    private boolean mShouldRequestPhonePermission = false;
+    private int mNumPermissionsToRequest = 0;
+    private boolean mFlagHasStoragePermission = false;
+    private boolean mFlagHasPhonePermission = false;
+    private boolean mCriticalPermissionDenied = false;
+
     private InterstitialAd mWelcomeInterstitialAd;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        checkPermissions();
+    }
+
+    private void checkPermissions() {
+        if (checkSelfPermission(Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            mNumPermissionsToRequest++;
+            mShouldRequestPhonePermission = true;
+        } else {
+            mFlagHasPhonePermission = true;
+        }
+        if (checkSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            mNumPermissionsToRequest = mNumPermissionsToRequest + 2;
+            mShouldRequestStoragePermission = true;
+        } else {
+            mFlagHasStoragePermission = true;
+        }
+        if (mNumPermissionsToRequest != 0) {
+            buildPermissionsRequest();
+        } else {
+            handlePermissionsSuccess();
+        }
+    }
+
+    private void handlePermissionsSuccess() {
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+        if (!MKCenterApplication.getInstance().getDonationInfo().isAdvanced()) {
+            MobileAds.initialize(this, getString(R.string.app_id));
+            mWelcomeInterstitialAd = new InterstitialAd(this);
+            mWelcomeInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
+            mWelcomeInterstitialAd.loadAd(new AdRequest.Builder().build());
+            mWelcomeInterstitialAd.setAdListener(new AdListener() {
+                @Override
+                public void onAdLoaded() {
+                    mWelcomeInterstitialAd.show();
+                }
+            });
+        }
+    }
+
+    private void handlePermissionsFailure() {
+        if (!mFlagHasStoragePermission) {
+            Toast.makeText(this, R.string.no_storage_permissions, Toast.LENGTH_LONG).show();
+        } else if (!mFlagHasPhonePermission) {
+            Toast.makeText(this, R.string.no_phone_permissions, Toast.LENGTH_LONG).show();
+        }
+        finish();
+    }
+
+    private void buildPermissionsRequest() {
+        String[] permissionsToRequest = new String[mNumPermissionsToRequest];
+        int permissionsRequestIndex = 0;
+
+        if (mShouldRequestStoragePermission) {
+            permissionsToRequest[permissionsRequestIndex] = Manifest.permission.WRITE_EXTERNAL_STORAGE;
+            mIndexPermissionRequestStorageWrite = permissionsRequestIndex;
+            permissionsRequestIndex++;
+            permissionsToRequest[permissionsRequestIndex] = Manifest.permission.READ_EXTERNAL_STORAGE;
+            mIndexPermissionRequestStorageRead = permissionsRequestIndex;
+            permissionsRequestIndex++;
+        }
+
+        if (mShouldRequestPhonePermission) {
+            permissionsToRequest[permissionsRequestIndex] = Manifest.permission.READ_PHONE_STATE;
+            mIndexPermissionRequestPhone = permissionsRequestIndex;
+        }
+
+        requestPermissions(permissionsToRequest, PERMISSION_REQUEST_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if (mShouldRequestStoragePermission) {
+            if ((grantResults.length >= mIndexPermissionRequestStorageRead + 1) &&
+                    (grantResults[mIndexPermissionRequestStorageWrite] == PackageManager.PERMISSION_GRANTED) &&
+                    (grantResults[mIndexPermissionRequestStorageRead] == PackageManager.PERMISSION_GRANTED)) {
+                mFlagHasStoragePermission = true;
+            } else {
+                mCriticalPermissionDenied = true;
+            }
+        }
+        if (mShouldRequestPhonePermission) {
+            if ((grantResults.length >= mIndexPermissionRequestPhone + 1) &&
+                    (grantResults[mIndexPermissionRequestPhone] == PackageManager.PERMISSION_GRANTED)) {
+                mFlagHasPhonePermission = true;
+            } else {
+                mCriticalPermissionDenied = true;
+            }
+        }
+        if (mFlagHasStoragePermission && mFlagHasPhonePermission) {
+            handlePermissionsSuccess();
+        } else if (mCriticalPermissionDenied) {
+            handlePermissionsFailure();
+        }
+    }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
@@ -83,36 +212,6 @@ public class MainActivity extends AppCompatActivity
             if (fragment != null) {
                 fragment.onActivityResult(requestCode, resultCode, data);
             }
-        }
-    }
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        DrawerLayout drawer = findViewById(R.id.drawer_layout);
-        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
-                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.addDrawerListener(toggle);
-        toggle.syncState();
-
-        NavigationView navigationView = findViewById(R.id.nav_view);
-        navigationView.setNavigationItemSelectedListener(this);
-
-        if (!MKCenterApplication.getInstance().getDonationInfo().isAdvanced()) {
-            MobileAds.initialize(this, getString(R.string.app_id));
-            mWelcomeInterstitialAd = new InterstitialAd(this);
-            mWelcomeInterstitialAd.setAdUnitId(getString(R.string.interstitial_ad_unit_id));
-            mWelcomeInterstitialAd.loadAd(new AdRequest.Builder().build());
-            mWelcomeInterstitialAd.setAdListener(new AdListener() {
-                @Override
-                public void onAdLoaded() {
-                    mWelcomeInterstitialAd.show();
-                }
-            });
         }
     }
 
@@ -153,7 +252,6 @@ public class MainActivity extends AppCompatActivity
         return super.onOptionsItemSelected(item);
     }
 
-    @SuppressWarnings("StatementWithEmptyBody")
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -190,16 +288,8 @@ public class MainActivity extends AppCompatActivity
         return false;
     }
 
-    public Snackbar makeSnackbar(CharSequence text) {
-        return makeSnackbar(text, Snackbar.LENGTH_SHORT);
-    }
-
     public Snackbar makeSnackbar(@StringRes int resId) {
         return makeSnackbar(resId, Snackbar.LENGTH_SHORT);
-    }
-
-    public Snackbar makeSnackbar(CharSequence text, int duration) {
-        return Snackbar.make(findViewById(R.id.updater), text, duration);
     }
 
     public Snackbar makeSnackbar(@StringRes int resId, int duration) {
