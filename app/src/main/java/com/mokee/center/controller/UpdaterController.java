7244abade4889960a1b1bf0c1dea7319a5fb6910
Martincz Gao <martincz@mokeedev.com>
2019-12-29 06:59:04 +0800

    MoKeeCenter: Improve download request
    Change-Id: Ia4f548f61168f525c048bf82d17287ef257edf11
    Reviewed-on: https://mokeedev.review/c/MoKee/android_packages_apps_MoKeeCenter/+/45412
    Tested-by: 高志鹏 <martincz@mokeedev.com>
    Reviewed-by: 高志鹏 <martincz@mokeedev.com>
    (cherry picked from commit 671dc3d664414267a4e30813e698d54cf08d37e7)

--- app/src/main/java/com/mokee/center/controller/UpdaterController.java
+++ app/src/main/java/com/mokee/center/controller/UpdaterController.java
@@ -32,7 +32,7 @@ import android.util.Log;
 import com.lzy.okgo.OkGo;
 import com.lzy.okgo.exception.HttpException;
 import com.lzy.okgo.model.Progress;
-import com.lzy.okgo.request.PostRequest;
+import com.lzy.okgo.request.base.Request;
 import com.lzy.okserver.OkDownload;
 import com.lzy.okserver.download.DownloadListener;
 import com.lzy.okserver.download.DownloadTask;
@@ -225,9 +225,14 @@ public class UpdaterController {
 
     public void startDownload(String downloadId) {
         Log.d(TAG, "Starting " + downloadId);
-        PostRequest<File> request = OkGo.post(mAvailableUpdates.get(downloadId).getDownloadUrl());
-        if (MKCenterApplication.getInstance().getDonationInfo().isBasic()) {
-            request.params(OkGoUtil.buildParams(mContext));
+        Request<File, ? extends Request> request;
+        if (BuildInfoUtil.isIncrementalUpdate(downloadId)) {
+            request = OkGo.get(mAvailableUpdates.get(downloadId).getDownloadUrl());
+        } else {
+            request = OkGo.post(mAvailableUpdates.get(downloadId).getDownloadUrl());
+            if (MKCenterApplication.getInstance().getDonationInfo().isBasic()) {
+                request.params(OkGoUtil.buildParams(mContext));
+            }
         }
         DownloadTask task = OkDownload.request(mAvailableUpdates.get(downloadId).getName(), request)
                 .fileName(FileUtil.getPartialName(downloadId)).save()
@@ -242,7 +247,9 @@ public class UpdaterController {
 
         if (MKCenterApplication.getInstance().getDonationInfo().isBasic()) {
             if (downloadTask.progress.request != null) {
-                downloadTask.progress.request.params(OkGoUtil.buildParams(mContext));
+                if (!BuildInfoUtil.isIncrementalUpdate(downloadId)) {
+                    downloadTask.progress.request.params(OkGoUtil.buildParams(mContext));
+                }
             } else {
                 restartDownload(downloadId);
                 return;
