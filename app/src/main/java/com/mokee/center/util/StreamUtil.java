10cee5f981506d070e6b313db4de3bd544759430
Martincz Gao <martincz@mokeedev.com>
2022-07-17 14:39:06 +0800

    Add own formatDuration() method
    DateUtils.formatDuration() is not public, so create our own version.
    
    Change-Id: I3e564f80d00f007c0317b3c9a9a047e5109f4a2b
    Reviewed-on: https://mokeedev.review/c/MoKee/android_packages_apps_MoKeeCenter/+/51161
    Tested-by: Martincz Gao <martincz@mokeedev.com>
    Reviewed-by: Martincz Gao <martincz@mokeedev.com>
    (cherry picked from commit 6868974a9ecc058f736d10cd50cbe51c468f410d)
    Reviewed-on: https://mokeedev.review/c/MoKee/android_packages_apps_MoKeeCenter/+/51180

--- app/src/main/java/com/mokee/center/util/StreamUtil.java
+++ app/src/main/java/com/mokee/center/util/StreamUtil.java
@@ -17,6 +17,11 @@
 
 package com.mokee.center.util;
 
+import android.icu.text.MeasureFormat;
+import android.icu.text.MeasureFormat.FormatWidth;
+import android.icu.util.Measure;
+import android.icu.util.MeasureUnit;
+
 import com.mokee.utils.HashUtils;
 
 import java.io.BufferedInputStream;
@@ -25,10 +30,21 @@ import java.io.InputStream;
 import java.security.DigestInputStream;
 import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.util.Locale;
 
 public class StreamUtil {
 
     private static final int BUF_SIZE = 16 * 1024;
+
+    public static final long SECOND_IN_MILLIS = 1000;
+    public static final long MINUTE_IN_MILLIS = SECOND_IN_MILLIS * 60;
+    public static final long HOUR_IN_MILLIS = MINUTE_IN_MILLIS * 60;
+
+    public static final int LENGTH_LONG = 10;
+    public static final int LENGTH_MEDIUM = 20;
+    public static final int LENGTH_SHORT = 30;
+    public static final int LENGTH_SHORTER = 40;
+    public static final int LENGTH_SHORTEST = 50;
 
     public static String calculateMd5(InputStream inputSource) throws IOException {
         MessageDigest md;
@@ -47,4 +63,37 @@ public class StreamUtil {
         return HashUtils.toHex(md.digest());
     }
 
+    public static CharSequence formatDuration(long millis) {
+        return formatDuration(millis, LENGTH_LONG);
+    }
+
+    public static CharSequence formatDuration(long millis, int abbrev) {
+        final FormatWidth width;
+        switch (abbrev) {
+            case LENGTH_LONG:
+                width = FormatWidth.WIDE;
+                break;
+            case LENGTH_SHORT:
+            case LENGTH_SHORTER:
+            case LENGTH_MEDIUM:
+                width = FormatWidth.SHORT;
+                break;
+            case LENGTH_SHORTEST:
+                width = FormatWidth.NARROW;
+                break;
+            default:
+                width = FormatWidth.WIDE;
+        }
+        final MeasureFormat formatter = MeasureFormat.getInstance(Locale.getDefault(), width);
+        if (millis >= HOUR_IN_MILLIS) {
+            final int hours = (int) ((millis + 1800000) / HOUR_IN_MILLIS);
+            return formatter.format(new Measure(hours, MeasureUnit.HOUR));
+        } else if (millis >= MINUTE_IN_MILLIS) {
+            final int minutes = (int) ((millis + 30000) / MINUTE_IN_MILLIS);
+            return formatter.format(new Measure(minutes, MeasureUnit.MINUTE));
+        } else {
+            final int seconds = (int) ((millis + 500) / SECOND_IN_MILLIS);
+            return formatter.format(new Measure(seconds, MeasureUnit.SECOND));
+        }
+    }
 }
